a6502fbfb5eab33f5aa9011bae3fffe227e3a276
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2020-09-05 13:50:17 +0530

    V_0.1

---
+++ pattern-searching/ahocorasick/ahocorasick.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Max number of states in the matching machine.
+// Should be equal to the sum of the length of all keywords.
+const int MAXS = 500;
+
+//Maximum number of characters in input alphabet
+const int MAXC = 26;
+
+//OUTPUT FUNCTION IS IMPLEMENTED USING OUT[]
